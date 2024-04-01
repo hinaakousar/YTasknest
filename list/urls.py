be304462ae5879c1bec7104463447f4cdc9029b3
hinaakousar <hinakousar006@gmail.com>
2024-04-01 01:41:59 -0700

    i updated dashboard urls redirect from signin
     create a urls of tasknes

--- list/urls.py
+++ list/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from list.views import *
 
 urlpatterns = [
-    path('land/', home, name='login' ),
-    path('make', makepro.as_view())
+    # path('land/', home, name='login' ),
+    # path('make', makepro.as_view())
     
 ]
--- list/views.py
+++ list/views.py
@@ -2,8 +2,5 @@ from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from user.views import *
 
-def home (request):
-    return render(request, 'home.html')
-
 class makepro (TemplateView):
     template_name = "board.html"
